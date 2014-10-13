f65a54c4be701ab06966fc829a608bbb59de21b1
phillipou94 <phillipou@Phillips-MacBook-Pro.local>
2014-10-12 17:34:33 -0700

    added highlight get and retrieval

--- web.js
+++ web.js
@@ -13,3 +13,23 @@ var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+//get request
+app.get('/highlights',function(req,res){
+	var collection = db.collection("highlights")
+
+	collection.find({}).toArray(function(e,results){
+		if(e) res.status(500).send()
+			res.send(results)
+	})
+})
+//post request
+
+app.post('/highlights', function(req,rest){
+	var collection = db.collection("highlights")
+
+	collection.insert(req.body,{},function(e,results){
+		if (e) res.status(500).send()
+			res.send(results)
+	})
+})
