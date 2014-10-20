02315b8d79b704bd2a8f6aedca43803025035fcf
phillipou94 <phillipou@Phillips-MacBook-Pro.local>
2014-10-20 15:12:25 -0700

    reverted back to 1 parameter put

--- web.js
+++ web.js
@@ -129,29 +129,21 @@ app.delete('/users/:id', function(req,res){ //pass parameter id.
 })
 
 
-app.put('/users/:id/:id2', function(req,req2, res, next) {
+app.put('/users/:id', function(req, res, next) {
 
   var collection = db.collection('users')
 
   var str1 = "followingDictionary"; //key
-  var str2 = "followersDictionary";
+  //var str2 = req.params.array;
   //var variable = str1.concat(str2);
   
  var action = {};
- var action2 = {};
- console.log(req2.body)
+ console.log(req.body)
  action[str1] = req.body;
- action2[str2]= req2.body;
   collection.updateById(req.params.id, {$set: //inc for integers, set for strings
     {followingDictionary:req.body}
   }, {safe: true, multi: false}, function(e, result){
     if (e) res.status(500).send()
     res.send(req.body)
-  })
-  collection.updateById(req.params.id2, {$set: //inc for integers, set for strings
-    {followersDictionary:req2.body}
-  }, {safe: true, multi: false}, function(e, result){
-    if (e) res.status(500).send()
-    res.send(req2.body)
   })
 })
