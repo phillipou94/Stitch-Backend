77e4fe5fc9d903459bd8c4373b554050c84d00d6
phillipou94 <phillipou@Phillips-MacBook-Pro.local>
2014-10-20 11:02:28 -0700

    syntax error

--- web.js
+++ web.js
@@ -140,7 +140,7 @@ app.put('/users/:id', function(req, res, next) {
  console.log(req.body)
  action[str1] = req.body;
   collection.updateById(req.params.id, {$set: //inc for integers, set for strings
-    {followingDictionary:[{key:"ja", value:"value"}]}
+    {followingDictionary:{key:"ja", value:"value"}}
   }, {safe: true, multi: false}, function(e, result){
     if (e) res.status(500).send()
     res.send(req.body)
