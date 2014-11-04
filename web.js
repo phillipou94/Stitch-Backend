3777f6b5a26089a5ae7188358f9b3c745c85f7ac
phillipou94 <phillipou@Phillips-MacBook-Pro.local>
2014-11-04 11:40:13 -0800

    fixed typo

--- web.js
+++ web.js
@@ -213,7 +213,6 @@ app.post('/messages', function(req,res){
 
 app.get('/messages/:id',function(req,res){
 	var collection = db.collection("messages")
-	collectionName.find()
 	collection.find({"recipientID": { $in: [req.params.id ] }},{}).toArray(function(e,results){
 		console.log(e);
 		if(e) res.status(500).send()
