810e555b24ec45b95f3891d4cc57617d07aace8d
phillipou94 <phillipou@Phillips-MacBook-Pro-2.local>
2014-11-05 13:08:49 -0800

    addded another console log

--- web.js
+++ web.js
@@ -55,6 +55,7 @@ app.post('/highlights/:serverID', function(req,res){
 		if (e) res.status(500).send()
 		app.put('/users', function(req, res, next) {
 			console.log("Put Request")
+			console.log(req.params.serverID)
 			var userCollection = db.collection("users")
 			var key = "arrayOfHighlights"; //key
 			var array = [];
