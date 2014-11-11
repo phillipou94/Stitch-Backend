18e2803d64778b336f0430da2692e7c6771f9dd7
phillipou94 <phillipou@Phillips-MacBook-Pro-2.local>
2014-11-11 11:03:32 -0800

    fixed query

--- web.js
+++ web.js
@@ -65,10 +65,10 @@ app.get('/highlights/:id/favorites',function(req,res){
 	//check to see if userID is a key in dictionary by seeing if it returns a value
 	//return everything not equal to null ($ne:null)
 	
-	query["favoritedByUsers"][req.params.id] = { $ne: null }
+	query = {favoritedByUsers[req.params.id] : {$exists : true}}
 	
 	console.log(query)
-	collection.find(query, {"quote":1, "articleTitle":1, "url":1, "username":1, "userID":1, "favoritedByUsers":1}).sort({dateCreated:1}).toArray(function(e,results){
+	collection.find(query, {"quote":1, "articleTitle":1, "url":1, "username":1, "userID":1, "favoritedByUsers":1,}).sort({dateCreated:1}).toArray(function(e,results){
 		if(e) res.status(500).send()
 		res.send(results)
 	})
