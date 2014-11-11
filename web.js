abcb4fc953bbb80d7170573a9902ab8d9c78edcd
phillipou94 <phillipou@Phillips-MacBook-Pro-2.local>
2014-11-11 10:10:29 -0800

    check using if statement

--- web.js
+++ web.js
@@ -65,7 +65,9 @@ app.get('/highlights/:id/favorites',function(req,res){
 	var key = "favoritedByUsers"
 	//check to see if userID is a key in dictionary by seeing if it returns a value
 	//return everything not equal to null ($ne:null)
-	query["favoritedByUsers"][req.params.id] = { $ne: null }
+	if(query["favoritedByUsers"]){
+		query["favoritedByUsers"][req.params.id] = { $ne: null }
+	}
 	console.log(query)
 	collection.find(query, {"quote":1, "articleTitle":1, "url":1, "username":1, "userID":1, "favoritedByUsers":1}).sort({dateCreated:1}).toArray(function(e,results){
 		if(e) res.status(500).send()
