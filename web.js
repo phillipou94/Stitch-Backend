fea0ddbe06062931d459bc257992e3028c43ee64
phillipou94 <phillipou@Phillips-MacBook-Pro-2.local>
2014-11-12 09:55:31 -0800

    syntax error

--- web.js
+++ web.js
@@ -49,7 +49,7 @@ app.get('/highlights',function(req,res){
 
 app.get('/highlights/random',function(req,res){
 	var collection = db.collection("highlights")
-	collection.findRandom({}, {"quote":1, "articleTitle":1, "url":1, "username":1, "userID":1, "favoritedByUsers":1}).limit(1).toArray(function(e,results){
+	collection.find({}, {"quote":1, "articleTitle":1, "url":1, "username":1, "userID":1, "favoritedByUsers":1}).limit(1).toArray(function(e,results){
 		if(e) res.status(500).send()
 		res.send(results)
 	})
